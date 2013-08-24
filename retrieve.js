095a3438189e95094741eb080b132f3d7f7adb89
Uli Fuchs <ufuchs@gmx.com>
2013-08-24 19:07:45 +0200

    finished: 'getCliToolsUrl'

--- retrieve.js
+++ retrieve.js
@@ -1,26 +1,124 @@
 /*jslint bitwise:true*/
 
+/*!
+ * retrieve
+ * Copyright(c) 2013 Uli Fuchs <ufuchs@gmx.com>
+ * MIT Licensed
+ *
+ * [ The true sign of intelligence is not knowledge but imagination. ]
+ * [                                             - Albert Einstein - ]
+ *
+ *
+ * Invoke at your Windows console:
+ * cscript //NoLogo this_filename
+ *
+ */
 
 'use strict';
 
+// ////////////////////////////////////////////////////////////////////////////
+// configuration
+// ////////////////////////////////////////////////////////////////////////////
+
 var packages = {
 
-    "all": {
-        "graphviz" : "http://www.graphviz.org/pub/graphviz/stable/windows/graphviz=graphviz-2.32.zip",
-        "cbmc": "http://www.cprover.org/cbmc/download/cbmc-4-4-win.zip"
-    },
 
-    "x86" : {
-        "nusmv": "http://nusmv.fbk.eu/distrib/NuSMV-2.5.4-i386-pc-mingw32.zip",
-        "yices": "http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.39-i686-pc-mingw32.zip"
-    },
+    "cliTools" : {
 
-    "x86_64" : {
-        "nusmv": "http://nusmv.fbk.eu/distrib/NuSMV-2.5.4-x86_64-w64-mingw32.zip",
-        "yices": "http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.39-x86_64-pc-mingw32.zip"
+
+        "installDir" : "",
+
+
+        "packages" : {
+
+            "all": {
+                "graphviz" : "http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.32.zip",
+                "cbmc": "http://www.cprover.org/cbmc/download/cbmc-4-4-win.zip"
+            },
+
+            "x86" : {
+                "nusmv": "http://nusmv.fbk.eu/distrib/NuSMV-2.5.4-i386-pc-mingw32.zip",
+                "yices": "http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.39-i686-pc-mingw32.zip"
+            },
+
+            "amd64" : {
+                "nusmv": "http://nusmv.fbk.eu/distrib/NuSMV-2.5.4-x86_64-w64-mingw32.zip",
+                "yices": "http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.39-x86_64-pc-mingw32.zip"
+            }
+        }
+
     }
 
 };
+
+// ////////////////////////////////////////////////////////////////////////////
+// common function
+// ////////////////////////////////////////////////////////////////////////////
+
+//
+//
+//
+function processorArch() {
+
+    var WshShell = new ActiveXObject("WScript.Shell"),
+        regKey = "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\\PROCESSOR_ARCHITECTURE";
+
+    return WshShell.RegRead(regKey);
+
+}
+
+// ////////////////////////////////////////////////////////////////////////////
+// cli tools
+// ////////////////////////////////////////////////////////////////////////////
+
+//
+//
+//
+function getIndependentCliToolsUrl() {
+    return [packages.cliTools.packages.all.graphviz, packages.cliTools.packages.all.cbmc];
+}
+
+//
+//
+//
+function getX86CliToolsUrl() {
+
+    var independentCliToolsUrl = getIndependentCliToolsUrl(),
+        x86CliToolsUrl = [packages.cliTools.packages.x86.nusmv, packages.cliTools.packages.x86.yices];
+
+    return independentCliToolsUrl.concat(x86CliToolsUrl);
+}
+
+//
+//
+//
+function getAmd64CliToolsUrl() {
+
+    var independentCliToolsUrl = getIndependentCliToolsUrl(),
+        amd64CliToolsUrl = [packages.cliTools.packages.amd64.nusmv, packages.cliTools.packages.amd64.yices];
+
+    return independentCliToolsUrl.concat(amd64CliToolsUrl);
+
+}
+
+//
+//
+//
+function getCliToolsUrl() {
+
+    var arch = processorArch(),
+        url;
+
+
+    if (arch === 'AMD64') {
+        url = getAmd64CliToolsUrl();
+    } else {
+        url = getX86CliToolsUrl();
+    }
+
+    return url;
+
+}
 
 //
 //
@@ -40,23 +138,30 @@ function extractPackageNameFromUrl(url) {
 //
 //
 //
-function writeResponseBody (responseBody, fromUrl) {
+function removePackage(packageName) {
 
-    var adoStream,
-        fileSystem,
-        packageName = extractPackageNameFromUrl(fromUrl);
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
 
-    adoStream = CreateObject("ADODB.Stream");
+    if (fso.FileExists(packageName)) {
+        fso.DeleteFile(packageName);
+    }
+
+
+}
+
+//
+//
+//
+function writePackageContent(res, packageName) {
+
+    var adoStream;
+
+    adoStream = new ActiveXObject("ADODB.Stream");
     adoStream.Open();
     adoStream.Type = 1;
-    adoStream.Write(responseBody);
+    adoStream.Write(res);
+
     adoStream.Position = 0;
-
-    fileSystem = CreateObject("Scripting.FileSystemObject");
-
-    if (fileSystem.FileExists(packageName)) {
-        fileSystem.DeleteFile(packageName);
-    }
 
     adoStream.SaveToFile(packageName);
     adoStream.Close();
@@ -66,12 +171,13 @@ function writeResponseBody (responseBody, fromUrl) {
 //
 //
 //
-function getText(url) {
+function getText(url, callback) {
 
     var strResult,
         status,
         http,
-        adoStream;
+        packageName = extractPackageNameFromUrl(url);
+
 
     try {
         // Create the WinHTTPRequest ActiveX Object.
@@ -90,7 +196,8 @@ function getText(url) {
             WScript.Quit(1);
         }
 
-        writeResponseBody(http.ResponseBody, url);
+//        writePackageContent(http.ResponseBody, packageName);
+        callback(http.ResponseBody, packageName);
 
 
     } catch (err) {
@@ -104,6 +211,12 @@ function getText(url) {
     return strResult;
 }
 
-var str = packages.x86.yices;
 
-WScript.Echo(extractPackageNameFromUrl(str));
+//WScript.Echo(getCliToolsUrl());
+
+
+getText("http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.32.zip", writePackageContent);
+
+//var str = packages.x86.yices;
+
+//WScript.Echo(extractPackageNameFromUrl(str));
