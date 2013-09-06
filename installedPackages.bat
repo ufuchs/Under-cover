686d5c43809bf5d6b90061d2599c149e8a2ffa2d
Uli Fuchs <ufuchs@gmx.com>
2013-09-07 01:03:28 +0200

    source commented

--- installedPackages.bat
+++ installedPackages.bat
@@ -1,12 +1,21 @@
+::
+:: installedPackages
+:: Copyright(c) 2013 Uli Fuchs <ufuchs@gmx.com>
+:: MIT Licensed
+::
+
 @ECHO OFF
 
+SETLOCAL
+
 SET packages_arch=packages-arch.txt
+SET packages_by_user=packages-by-user.txt
 
 :: ONLY VALID for AMD64 systems and only exists on them.
 :: Contains all installed 32-bit packages on an AMD64 system.
 SET regKey_wow6432node=HKLM\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 
-:: Contains all packages for a given architecture, e.g. x86 or AMD64
+:: Contains the packages for a given architecture, e.g. x86 or AMD64
 :: - On AMD64 systems this key contains one and only the installed 64-bit
 ::   programs and _not_ the 32bit/x86 programs.
 :: - On x86 systems this key contains all installed programs.
@@ -14,59 +23,68 @@ SET regKey_Arch=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
 
 GOTO :MAIN
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Gets the content from an given registry key and write it to the given file.
 ::
 :: @param1 {regKey} String
 :: @param2 {filename} String
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :GET_PACKAGES_BY_REGKEY
 
-    SETLOCAL
-
+    :: drop the fields at position 1 and 2
     FOR /F "tokens=1,2* delims= " %%a IN ('reg query %1 /s ^| findstr /B ".*DisplayName" ') DO (
         @ECHO %%c >> %2
     )
 
-    ENDLOCAL
     GOTO :eof
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Gets the installed packages on an X86 system
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:GET_PACKAGES_ON_x86
+
+    :: x86(32-bit) packages
+    CALL :GET_PACKAGES_BY_REGKEY %regKey_arch% %packages_arch%
+
+    GOTO :eof
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Gets the installed packages on an AMD64 system
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :GET_PACKAGES_ON_AMD64
 
-    SETLOCAL
-
+    :: x86(32-bit) packages
     CALL :GET_PACKAGES_BY_REGKEY %regKey_wow6432node% %packages_arch%
+
+    :: AMD64(64-bit) packages
     CALL :GET_PACKAGES_BY_REGKEY %regKey_arch% %packages_arch%
 
-    ENDLOCAL
     GOTO :eof
 
-:: Gets the installed packages on an X86 system
-:GET_PACKAGES_ON_X86
-
-    SETLOCAL
-
-    CALL :GET_PACKAGES_BY_REGKEY %regKey_arch% %packages_arch%
-
-    ENDLOCAL
-    GOTO :eof
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Gets the installed packages from a given architecture
 :: @param1 {architecture} - AMD64 or x86
-:GET_PACKAGES
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:GET_ALL_INSTALLED_PACKAGES
 
-    SETLOCAL
-
+    :: tidy up
     DEL %packages_arch% > NUL 2>&1
+    DEL %packages_by_user% > NUL 2>&1
+
     CALL :GET_PACKAGES_ON_%1
 
-    ENDLOCAL
+    :: drop Microsoft/Intel related packages
+    FOR /F "tokens=*" %%a IN ('type %packages_arch% ^| findstr /V "Intel" ^| findstr /V "Microsoft" ^| findstr /V "C++" ^| findstr /V "SQL" ^| findstr /V "@"' ) DO (
+        @ECHO %%a >> %packages_by_user%
+    )
+
     GOTO :eof
 
 ::::::::::::::::::::::::::::::::::::::::
 :MAIN
 ::::::::::::::::::::::::::::::::::::::::
 
-CALL :GET_PACKAGES AMD64
+CALL :GET_ALL_INSTALLED_PACKAGES AMD64
 
-
+ENDLOCAL
 
