dffa98ea0265e331d6dd249206721062ae6bbbde
Uli Fuchs <ufuchs@gmx.com>
2013-09-07 22:30:35 +0200

    added: read from file into variable

--- research.bat
+++ research.bat
@@ -1,5 +1,7 @@
 @echo off
 
+GOTO :RUN
+
 :: http://stackoverflow.com/questions/8493493/how-to-loop-thorough-tokens-in-a-string
 setlocal EnableDelayedExpansion
 set "str=foo bar:biz bang:this & that "^& the other thing!":;How does this work?"
@@ -11,4 +13,14 @@ for /f "eol=: delims=" %%S in ("!str!") do (
   if "!!"=="" endlocal
   echo %%S
 )
+
+:RUN
+
+:: http://stackoverflow.com/questions/3068929/how-to-read-file-contents-into-a-variable-in-a-batch-file
+SETLOCAL EnableDelayedExpansion
+
+set Build=
+for /f "Tokens=* Delims=" %%x in ('type packages-by-user.txt') do set Build=!Build!%%x:
+
+echo %Build%
 
