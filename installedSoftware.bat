360df7b3a2e9fd3303170b84d642a8b3a303c4ed
Uli Fuchs <ufuchs@gmx.com>
2013-09-10 20:26:41 +0200

    refactoring

--- installedSoftware.bat
+++ installedSoftware.bat
@@ -8,8 +8,9 @@
 
 SETLOCAL
 
+SET platform_arch=
 SET software_all=software-all.txt
-SET software_by_user=software-by-user.txt
+SET software_in_scope=software-by-user.txt
 SET software_to_search=software-to-search.txt
 
 :: ONLY VALID for AMD64 systems and only exists on them.
@@ -29,6 +30,18 @@ GOTO :MAIN
 ::
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Aquire the platform architecture. Gets 'AMD64' or 'x86'
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:GET_PLATFORM_ARCHITECTURE
+
+    FOR /f "tokens=2* delims= " %%a IN ('reg query ^
+        "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" ^
+        /v ^
+        "PROCESSOR_ARCHITECTURE"') DO SET platform_arch=%%b
+
+    GOTO :eof
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Gets the content from a given registry key and write it to the given file.
 ::
 :: @param1 {regKey} String
@@ -46,9 +59,9 @@ GOTO :MAIN
     GOTO :eof
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Gets the installed packages on an X86 system
+:: Gets _all_ installed software packages on an X86 system
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:GET_SOFTWARE_ON_x86
+:GET_ALL_SOFTWARE_PACKAGES_FOR_x86
 
     :: x86(32-bit) packages
     CALL :GET_SOFTWARE_BY_REGKEY %regKey_arch% %software_all%
@@ -56,9 +69,9 @@ GOTO :MAIN
     GOTO :eof
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Gets the installed packages on an AMD64 system
+:: Gets _all_ installed software packages on an AMD64 system
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:GET_SOFTWARE_ON_AMD64
+:GET_ALL_SOFTWARE_PACKAGES_FOR_AMD64
 
     :: x86(32-bit) packages
     CALL :GET_SOFTWARE_BY_REGKEY %regKey_wow6432node% %software_all%
@@ -69,19 +82,11 @@ GOTO :MAIN
     GOTO :eof
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Gets the installed packages from a given architecture
-:: @param1 {architecture} - AMD64 or x86
+:: Reading from the registry gets duplicated enties.
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:GET_SOFTWARE_BY_USER
+:REMOVE_DUPLICATE_ENTRIES
 
     SETLOCAL enableDelayedExpansion
-
-    :: tidy up
-    DEL %software_all% > NUL 2>&1
-    DEL %software_by_user% > NUL 2>&1
-    DEL temp.txt > NUL 2>&1
-
-    CALL :GET_SOFTWARE_ON_%1
 
     SORT %software_all% /o %software_all%
 
@@ -95,9 +100,24 @@ GOTO :MAIN
           SET prevLine=!line!
         )
     )
+
     COPY /V /Y temp.txt %software_all% > NUL 2>&1
 
-    :: drop Microsoft/Intel related packages
+    DEL temp.txt
+
+    ENDLOCAL
+
+    GOTO :eof
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: To increase the performance, all software entries out of scope/interest
+:: should be removed.
+:: The output is written into %software_in_scope%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:EXCLUDE_SOFTWARE_OUT_OF_SCOPE
+
+    SETLOCAL
+
     FOR /F "tokens=*" %%a IN ('type %software_all% ^
         ^| findstr /V "Intel" ^
         ^| findstr /V "Microsoft"
@@ -105,24 +125,42 @@ GOTO :MAIN
         ^| findstr /V "SQL" ^
         ^| findstr /V "@"' ) DO (
         :: A space between '%%c >>' writes an extra space at line end
-        ECHO %%a>> %software_by_user%
+        ECHO %%a>> %software_in_scope%
     )
 
     ENDLOCAL
+
+    GOTO :eof
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Gets the software packages which should be scanned.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:GET_SOFTWARE_IN_SCOPE
+
+    SETLOCAL enableDelayedExpansion
+
+    :: delete files from last run
+    DEL %software_all% > NUL 2>&1
+    DEL %software_in_scope% > NUL 2>&1
+
+    CALL :GET_PLATFORM_ARCHITECTURE
+
+    CALL :GET_ALL_SOFTWARE_PACKAGES_FOR_%platform_arch%
+
+    CALL :REMOVE_DUPLICATE_ENTRIES
+
+    CALL :EXCLUDE_SOFTWARE_OUT_OF_SCOPE
+
+    ENDLOCAL
+
     GOTO :eof
 
 ::::::::::::::::::::::::::::::::::::::::
 :MAIN
 ::::::::::::::::::::::::::::::::::::::::
 
-:: Aquire the platform architecture. Gets 'AMD64' or 'x86'
-FOR /f "tokens=2* delims= " %%a IN ('reg query ^
-    "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" ^
-    /v ^
-    "PROCESSOR_ARCHITECTURE"') DO SET arch=%%b
-
 :: Fetch the installed software
-CALL :GET_SOFTWARE_BY_USER %arch%
+CALL :GET_SOFTWARE_IN_SCOPE
 
 SETLOCAL enableDelayedExpansion
 
@@ -140,7 +178,7 @@ FOR /f "Delims=" %%x IN ('type %software_to_search%') DO (
 
 :: look up for the software to search
 SET "sp=%sts%"
-FOR /F "tokens=*" %%a IN ('type %software_by_user%') DO (
+FOR /F "tokens=*" %%a IN ('type %software_in_scope%') DO (
     :: iterates over the software names in the search string for each installed software package
     FOR %%S IN ("%sp::=" "%") DO (
         ECHO %%a | findstr /B "%%~S"
